bcc1b0fbff42c0e22bd9397423500492c020eb2b
Droxinia <dagmawishimelis2@gmail.com>
2023-03-26 01:44:57 +0300

    First commit

---
+++ lib/Screens/Sign_in.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Sign_in extends StatelessWidget {
+  const Sign_in({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Screens/Suplements.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Suplements extends StatelessWidget {
+  const Suplements({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
