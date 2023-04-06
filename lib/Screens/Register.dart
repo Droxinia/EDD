87ca83ff948129c77ae4623bc70e405e91c06a75
Droxinia <dagmawishimelis2@gmail.com>
2023-04-06 14:39:06 +0300

    changes made

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -1,13 +1,78 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class Register extends StatelessWidget {
+import 'package:animated_button/animated_button.dart';
+import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+
+import '../signinform.dart';
+
+class Register extends StatefulWidget {
   const Register({super.key});
 
   @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("welcome to registery"),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 430, 0, 0),
+        child: Column(
+          children: [
+            AnimatedButton(
+              onPressed: () {
+                showGeneralDialog(
+                    barrierDismissible: true,
+                    barrierLabel: "sign in",
+                    context: context,
+                    pageBuilder: (context, animation, secoundaryanimation) =>
+                        Center(
+                          child: Container(
+                            height: 620,
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 29, vertical: 30),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 32, horizontal: 24),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40))),
+                            child: Scaffold(
+                              backgroundColor: Colors.transparent,
+                              body: Column(
+                                children: const [
+                                  Text(
+                                    "Sign In",
+                                    style: TextStyle(
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      vertical: 16,
+                                    ),
+                                    child: Text(
+                                      "View 1000's of people's story and give out a helping hand. we are all one people of one nation use our paltform to get other stories out there join us on our journey today",
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  Signin()
+                                ],
+                              ),
+                            ),
+                          ),
+                        ));
+              },
+              child: const Text("Sign in"),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            AnimatedButton(onPressed: () {}, child: const Text("Sign Up"))
+          ],
+        ),
       ),
     );
   }
---
+++ lib/signinform.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  const Signin({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Username",
+          style: TextStyle(color: Colors.black54),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8, bottom: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+                prefixIcon: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Icon(Icons.person),
+            )),
+          ),
+        ),
+        const Text(
+          "Password",
+          style: TextStyle(color: Colors.black54),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8, bottom: 16),
+          child: TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+                prefixIcon: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Icon(Icons.key),
+            )),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 24),
+          child: ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.lightBlue,
+                  minimumSize: const Size(double.infinity, 56),
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                          bottomLeft: Radius.circular(25)))),
+              icon: const Icon(Icons.arrow_right),
+              label: const Text("Sign in")),
+        )
+      ],
+    ));
+  }
+}
