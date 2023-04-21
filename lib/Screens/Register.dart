bc629c94280fcca10a5b0cf365b5f947983dbd36
YeabseraMesfin <ymesfin2001@gmail.com>
2023-04-21 05:29:12 -0700

    localization on registeration page done

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -19,6 +19,78 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showGeneralDialog
+                  
+                  (barrierDismissible: true,barrierLabel: 'languages'.tr().toString(),context:context, pageBuilder: ((context, animation, secondaryAnimation) => Center(
+                    child: Container(
+                      height: 320,
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      padding: EdgeInsets.symmetric(vertical: 32,horizontal: 24),
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(40)
+                      ),
+                      child: Scaffold(
+                        backgroundColor: Colors.transparent,
+                        body: Center(
+                          child: Column(children: [
+                            Text('languages'.tr().toString(), style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                            Padding( 
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: Text('change-language'.tr().toString(),textAlign: TextAlign.center,),
+                            ),
+                           GestureDetector(
+                            onTap: () => context.locale = const Locale('en','US'),   
+                             child: Container(
+                              height: 50,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                color: Colors.white60,
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              child: Center(child: Row(
+                                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [CircleAvatar(backgroundImage: AssetImage('assets/images/1.jpg'), radius: MediaQuery.of(context).size.height *0.02,),
+                                  Text('English'),
+                                ],
+                              )),
+                             ),
+                           ),
+                           SizedBox(height: 10,),
+                           GestureDetector(
+                            onTap: () => context.locale = const Locale('am','ETH'),
+                             child: Container(
+                              height: 50,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                
+                                color: Colors.white60,
+                                borderRadius: BorderRadius.circular(40),
+                              ),
+                              child: Center(child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [CircleAvatar(backgroundImage: AssetImage('assets/images/2.jpg'), radius: MediaQuery.of(context).size.height *0.02,),
+                                  Text('amharic'.tr().toString()),
+                                ],
+                              ),
+                              ),
+                             ),
+                           ),
+                          ],),
+                        ),
+                      ),
+                    ),
+                  )
+                  ),
+                  );
+        },
+        child: Icon(Icons.language),
+      ),
+      
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 430, 0, 0),
         child: Column(
